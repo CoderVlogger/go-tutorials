da3606b9d0f9fe1750a60edaf6b211d5b8da051e
Kanan Rahimov <mail@kenanbek.me>
2020-03-15 05:09:52 +0100

    goweb - gin - GET, POST, DELETE, LIST

--- goweb/gowebgin/main.go
+++ goweb/gowebgin/main.go
@@ -1,13 +1,44 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	mdb := NewMemDb()
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
+	})
+
+	r.GET("/users", func(c *gin.Context) {
+		c.JSON(http.StatusOK, mdb.ListUsers())
+	})
+
+	r.POST("/users", func(c *gin.Context) {
+		var user User
+		err := c.BindJSON(&user)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, err.Error())
+		} else {
+			c.JSON(http.StatusCreated, mdb.AddUser(&user))
+		}
+	})
+
+	r.GET("/users/:username", func(c *gin.Context) {
+		username := c.Param("username")
+		c.JSON(http.StatusOK, mdb.GetUser(username))
+	})
+
+	r.DELETE("/users/:username", func(c *gin.Context) {
+		username := c.Param("username")
+		mdb.DeleteUser(username)
+		c.JSON(http.StatusOK, gin.H{})
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
